70128d8431ba8d5ab8181a2bac793b771e97b5b9
Mansi Verma <vermamansi3003@gmail.com>
2025-04-12 14:09:15 +0530

    RoomVista

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Editor from "./pages/Editor";
-import UserManual from "./components/UserManual";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/editor" element={<Editor />} />
-        <Route path="/user" element={<UserManual/>} />
       </Routes>
     </Router>
   );
